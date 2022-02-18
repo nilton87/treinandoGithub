03574ca61a177b974a2ddf27a636ccbb64eff001
Nilton Sousa <civil.engsilva@gmail.com>
2022-02-18 16:59:55 -0300

    inserção de um exemplo de desestruturação

--- arquivoteste.js
+++ arquivoteste.js
@@ -3,3 +3,19 @@ let filtrarNumerosPares = (array) => array.filter((num) => num%2 ==0);
 
 console.log(filtrarNumerosPares(todosOsNumeros));
 
+//Usando atribuição de desestruturação para extrair valores de objetos:
+
+const temperatura = {
+    yesterday: 28,
+    today: 32,
+    tomorrow: 25
+  };
+  
+ 
+  const {yesterday} = temperatura;
+  const {today} = temperatura;
+  const {tomorrow}= temperatura;
+   
+  console.log(temperatura);
+
+  
